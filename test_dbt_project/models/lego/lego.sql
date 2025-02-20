f029c2b0a02373b93be3af83e4695f5e6bd0039c
manojach87 <manoj.ach@gmail.com>
2025-02-19 19:57:15 -0500

    dbt changes

--- test_dbt_project/models/lego/lego.sql
+++ test_dbt_project/models/lego/lego.sql
@@ -1,13 +1,8 @@
-WITH UNIQUE_PARTS AS (
-SELECT 
-	P.part_num
-FROM parts as P
-INNER JOIN inventory_parts as IP on P.part_num = IP.part_num
-INNER JOIN inventories as I on I.id = IP.inventory_id
-INNER JOIN sets as S on S.set_num = I.set_num
-	GROUP BY P.part_num
-	HAVING COUNT(*) = 1
-)
+-- WITH UNIQUE_PARTS AS (
+-- SELECT *
+-- FROM {{ ref('unique_parts') }}
+-- -- WHERE -- your conditions
+-- )
 SELECT 
 	T.name as theme_name,
 	S.name as set_name,
@@ -18,9 +13,9 @@ SELECT
 	END as unique_part,
 	COUNT(P.part_num) as parts
 FROM parts as P
-LEFT JOIN UNIQUE_PARTS as UP on P.part_num = UP.part_num
+LEFT JOIN {{ ref('unique_parts') }} as UP on P.part_num = UP.part_num
 INNER JOIN inventory_parts as IP on P.part_num = IP.part_num
 INNER JOIN inventories as I on I.id = IP.inventory_id
 INNER JOIN sets as S on S.set_num = I.set_num
 INNER JOIN themes as T on T.id = S.theme_id
-GROUP BY 1,2,3,4;
+GROUP BY 1,2,3,4
